b1596853757bd2f147791721106eb2d78187bcc4
Fortune Uwha <fortune.uwha@gmail.com>
2021-07-07 22:45:00 +0100

    Update app.py

--- docker/app.py
+++ docker/app.py
@@ -3,7 +3,7 @@ from calculator.calculator import Calculator
 calculator = Calculator()
 
 result = calculator.add(10)
-print(f"0+4 = {result}")
+print(f"0 + 10 = {result}")
 
 result = calculator.subtract(3)
 print(f"value_in_memory - 3 = {result}")
