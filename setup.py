7126244a86e24537fb9ca0c8137ac60d35684d05
Fortune Uwha <fortune.uwha@gmail.com>
2021-07-07 11:42:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,23 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="calculator",
-    version="0.0.1",
-    description="Turing College 2.1 sprint project",
+    name='calpy',
+    version='0.0.1',
+    license='MIT',
+    description='This calculator can be used for simple arithmetic operations.',
+    author='Fortune Uwha',
+    author_email='fortune.uwha@gmail.com',
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url='https://github.com/fortune-uwha/calculator',
+    download_url='https://github.com/fortune-uwha/calculator/archive/refs/tags/v0.1.tar.gz',
     packages=setuptools.find_packages(),
+    keywords=['calculator', 'maths', 'arithmetic'],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
